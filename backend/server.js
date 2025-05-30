67777a0e395d7259f063151097e1565ea37fa15e
rahul <114241667+rahulsai4@users.noreply.github.com>
2025-05-30 18:14:59 +0530

    notifications routes done

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 import { connectDB } from "./db/connectDB.js";
-import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
